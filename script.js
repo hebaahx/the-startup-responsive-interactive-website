75824a31cc46aedba953c4977f27481cdff87dc0
handouh1 <heba.handouka@hva.nl>
2026-01-21 02:03:53 +0100

    #6 animation change

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // STAP 1: selecteren wat ik nodig heb de form, buttonen input
 
 const form = document.querySelector('form')
-const button = document.querySelector('form button')
+const buttonText = document.querySelector('.button-text')
+const button = document.querySelector('.submit-button')
 const input = document.querySelector('#email')
 
 //STAP2: luistreren naar submit, wachten op klik
@@ -9,8 +10,13 @@ form.addEventListener('submit', function(event){
     event.preventDefault()
 
 
-//STAP3: actie uitvoeren + animatie
-button.classList.add('rotate')
-button.textContent = 'Signed Up' //tekst veranderen
+//STAP3: animatie starten
+buttonText.classList.add('rotate-text')
 input.disabled = true // input uitschakelen 
-});
+})
+
+// na animatie moet de tekst en kleur veranderen
+buttonText.addEventListener('animationend', function(){
+    buttonText.textContent = 'Signed Up ✔' //tekst veranderen
+    button.classList.add('button-succes') //button kleur
+})
